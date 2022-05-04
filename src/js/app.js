9e11d77992e278a90cdfbd4eb6dcbee9c444cc6f
cerm88 <cerm1202@hotmail.com>
2022-05-04 00:47:32 -0400

    feat: carga de lista de películas en tendencias

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,44 @@
 import env from './ENV/env.js';
 
-console.log(env.API_KEY);
+const API_KEY = env.API_KEY;
+const URL_IMG_BASE = 'https://image.tmdb.org/t/p/w300';
 
-console.log(env.API_TOKEN);
+const URL_TRENDING = (mediaType, timeWindow) => {
+    const params = [
+        `https://api.themoviedb.org/3/trending/${mediaType}/${timeWindow}`,
+        '?',
+        `api_key=${API_KEY}`,
+    ];
+    return params.join('');
+};
+
+const errorNode = document.querySelector('#error');
+const rendingPreviewMoviesContainer = document.querySelector(
+    '#trendingPreview .trendingPreview-movieList',
+);
+
+async function getTrendingMoviesPreview() {
+    try {
+        const response = await fetch(URL_TRENDING('movie', 'day'));
+        const status = response.status;
+        if (status !== 200) throw new Error(`Error en la petición GET. Código HTTP: ${status}`);
+        const data = await response.json();
+        const movies = data.results;
+        movies.forEach((movie) => {
+            const movieContainer = document.createElement('div');
+            const movieImage = document.createElement('img');
+            movieContainer.classList.add('movie-container');
+            movieImage.classList.add('movie-img');
+            movieImage.src = `${URL_IMG_BASE}${movie.poster_path}`;
+            movieImage.alt = movie.title;
+            movieContainer.appendChild(movieImage);
+            rendingPreviewMoviesContainer.appendChild(movieContainer);
+        });
+    } catch (error) {
+        const msgError = `Error: ${error.message}`;
+        const nodeTextError = document.createTextNode(msgError);
+        errorNode.appendChild(nodeTextError);
+    }
+}
+
+getTrendingMoviesPreview();
