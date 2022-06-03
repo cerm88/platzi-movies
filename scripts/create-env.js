673b112c74fa548f6e655af5f468b8b5e395276e
cerm88 <cerm1202@hotmail.com>
2022-06-02 21:16:21 -0400

    fix: reparado error de creación de env mediante el script

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,10 +1,5 @@
 const fs = require('fs');
 
-const ENV = [
-    '# Llave API (v3 auth)',
-    `API_KEY=${process.env.API_KEY}\n`,
-    '# Token de acceso de lectura a la API (v4 auth)',
-    `API_TOKEN=${process.env.API_TOKEN}\n`,
-].join('\n');
+const ENV = `API_KEY=${process.env.API_KEY}\nAPI_TOKEN=${process.env.API_TOKEN}\n`;
 
 fs.writeFileSync('./.ENV', ENV);
